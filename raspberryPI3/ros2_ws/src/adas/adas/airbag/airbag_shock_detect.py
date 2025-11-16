30d407324a6fdc32c4cd3aff0b9ee66585fc791d
navilenza <57918839+navilenza@users.noreply.github.com>
2025-11-16 15:05:01 +0100

    End of Sprint 1
    * Package creation and node with early obstacle detection
    
    Builds, not tested
    
    * Fixed joystick error on car launch
    
    * Fixed publish motors_order
    
    * Stopping before obstacle validated
    
    Erasing unnecessary logging
    
    * add speed cap
    
    * Fix % in logger
    
    * Fix steering angle and types
    
    * adas package creation
    
    * Fix type error
    
    * Adas now a Python package
    
    * Rewrite in Python
    
    * Fix multiple issues - tested
    
    * [2.A] Airbag Shock Detection  (#1)
    
    * Airbag package creation
    
    * Shock detection with 1.5 as minimal value
    
    * Python naming convention harmonization
    
    * Gravity is not a shock
    
    * Print log only when shock detected
    
    * Added airbag detection to gei-car launch  and cleaning code
    
    * Airbag in adas package
    
    ---------
    
    Co-authored-by: pofpifpaf <adam.gironcel@gmail.com>
    Co-authored-by: zeuitcher <louismkeija@gmail.com>

---
+++ raspberryPI3/ros2_ws/src/adas/adas/airbag/airbag_shock_detect.py
@@ -0,0 +1,67 @@
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Imu
+from std_msgs.msg import Bool
+
+min_linear_acceleration = 2.5
+
+class airbag_shock_detection(Node):
+
+    def __init__(self):
+        #Initialization of the shock_detection_node Node
+        super().__init__('shock_detection_node')
+        self.publisher_ = self.create_publisher(Bool, 'isShockDetected', 10)
+
+        #Subscription to the IMU topic
+        self.subscription = self.create_subscription(Imu,'/imu/data', self.imu_callback,10)
+        self.subscription  # prevent unused variable warning
+
+
+    # function that get called when an IMU value is received and send in the topic isShockDetect
+    # true if val >= min_linear_acceleration
+    def imu_callback(self, msg):
+
+        # Define a variable with the minimal value for shock detection
+        
+        # get the data  from the imu topic
+        ax = msg.linear_acceleration.x
+        ay = msg.linear_acceleration.y
+
+        # Compare the value with the min we defined
+        if (ax >= min_linear_acceleration or 
+            ay >= min_linear_acceleration ):
+            shock = True
+
+            # log to debug
+            self.get_logger().info(f"Accel = ({ax:.2f}, {ay:.2f}), Shock detected")
+
+        else:
+            shock = False
+
+        # publish the message with shock state
+        shock_msg = Bool()
+        shock_msg.data = shock
+        self.publisher_.publish(shock_msg)
+
+
+
+
+ 
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    shock_detection_node = airbag_shock_detection()
+
+    rclpy.spin(shock_detection_node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    shock_detection_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()

ebb432638cbd74348630f80e573581a5503ebb93
Caroline Nguyen <caroline0084@gmail.com>
2025-11-29 17:11:57 +0100

    Deleted duplicate file, fixed bug, naming convention in Python (wsh Keija fait un effort)

--- raspberryPI3/ros2_ws/src/adas/adas/esp_node.py
+++ raspberryPI3/ros2_ws/src/adas/adas/esp_node.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 import math
 
-from interfaces.msg import MotorsOrder, sensor_msgs/boussole
+from interfaces.msg import MotorsOrder #, sensor_msgs/boussole
 
 class Esp(Node):
     def __init__(self):
@@ -10,7 +10,7 @@ class Esp(Node):
 
         self.publisher_motors_order = self.create_publisher(MotorsOrder, 'motors_order', 10)
         self.subscription = self.create_subscription(MotorsOrder,'motors_order_raw', self.motors_order_callback, 10)
-        self.subscription = self.create_subscription(sensor_msgs/boussole,'boussole/data_raw', self.boussole_callback, 10)
+        #self.subscription = self.create_subscription(sensor_msgs/boussole,'boussole/data_raw', self.boussole_callback, 10)
         self.subscription  # prevent unused variable warning
 
         self.motor_right_rear_pwm = 50
@@ -23,19 +23,19 @@ class Esp(Node):
 
         self.get_logger().info("Esp_node READY")
 
-def motors_order_callback(self, motors_order : MotorsOrder):
+    def motors_order_callback(self, motors_order : MotorsOrder):
 
-    self.motor_right_rear_pwm = motors_order.right_rear_pwm
-    self.motor_left_rear_pwm = motors_order.left_rear_pwm
-    self.motor_steering_angle = motors_order.steering_angle
+        self.motor_right_rear_pwm = motors_order.right_rear_pwm
+        self.motor_left_rear_pwm = motors_order.left_rear_pwm
+        self.motor_steering_angle = motors_order.steering_angle
 
 # remplacer par la boussole
-def boussole_callback(self, boussole : MotorsOrder):
-    self.Trajectory_control(self):
+#def boussole_callback(self, boussole : MotorsOrder):
+#    self.trajectory_control(self)
 
 
 
-def Trajectory_control(self):
+def trajectory_control(self):
     """if pas de probleme d'angle :
             msg = MotorsOrder()
             msg.right_rear_pwm = self.motor_right_rear_pwm
@@ -46,20 +46,20 @@ def Trajectory_control(self):
         else 
             angle actuel - angle de reference
             faire +/- 180"""
-        self.get_logger().info("Trajectory deviation: °")
+    self.get_logger().info("Trajectory deviation: °")
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    Esp_node = Esp()
+    esp_node = Esp()
 
-    rclpy.spin(Esp_node)
+    rclpy.spin(esp_node)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    Esp_node.destroy_node()
+    esp_node.destroy_node()
     rclpy.shutdown()
 
 

305f708747971aaf492c3d4ace9b2465b68d40fa
Zeuitcher <louismkeija@gmail.com>
2025-11-29 16:03:11 +0100

    Debut du node sans boussole

---
+++ raspberryPI3/ros2_ws/src/adas/adas/esp_node.py
@@ -0,0 +1,68 @@
+import rclpy
+from rclpy.node import Node
+import math
+
+from interfaces.msg import MotorsOrder, sensor_msgs/boussole
+
+class Esp(Node):
+    def __init__(self):
+        super().__init__('Esp_node')
+
+        self.publisher_motors_order = self.create_publisher(MotorsOrder, 'motors_order', 10)
+        self.subscription = self.create_subscription(MotorsOrder,'motors_order_raw', self.motors_order_callback, 10)
+        self.subscription = self.create_subscription(sensor_msgs/boussole,'boussole/data_raw', self.boussole_callback, 10)
+        self.subscription  # prevent unused variable warning
+
+        self.motor_right_rear_pwm = 50
+        self.motor_left_rear_pwm = 50
+        self.motor_steering_angle = 0
+
+        #en attendant la boussole
+        #self.boussole = angle actuel
+        #self.boussole = angle de reference
+
+        self.get_logger().info("Esp_node READY")
+
+def motors_order_callback(self, motors_order : MotorsOrder):
+
+    self.motor_right_rear_pwm = motors_order.right_rear_pwm
+    self.motor_left_rear_pwm = motors_order.left_rear_pwm
+    self.motor_steering_angle = motors_order.steering_angle
+
+# remplacer par la boussole
+def boussole_callback(self, boussole : MotorsOrder):
+    self.Trajectory_control(self):
+
+
+
+def Trajectory_control(self):
+    """if pas de probleme d'angle :
+            msg = MotorsOrder()
+            msg.right_rear_pwm = self.motor_right_rear_pwm
+            msg.left_rear_pwm = self.motor_left_rear_pwm
+            msg.steering_angle = self.motor_steering_angle
+            self.publisher_motors_order.publish(msg)
+            return
+        else 
+            angle actuel - angle de reference
+            faire +/- 180"""
+        self.get_logger().info("Trajectory deviation: °")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    Esp_node = Esp()
+
+    rclpy.spin(Esp_node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    Esp_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+    

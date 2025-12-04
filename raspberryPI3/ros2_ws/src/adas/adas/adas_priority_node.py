730ba12f153bd306716e554421e294b9f3a365c5
root <root@DESKTOP-B2FODNI.localdomain>
2025-12-05 00:45:17 +0100

    changes to HMIFeatures interface and priority_node to correctly send priorities to HMI

--- raspberryPI3/ros2_ws/src/adas/adas/adas_priority_node.py
+++ raspberryPI3/ros2_ws/src/adas/adas/adas_priority_node.py
@@ -2,7 +2,16 @@ import rclpy
 from rclpy.node import Node
 from interfaces.msg import MotorsOrder, MotorsOrderAdas, HmiFeatures
 
-#Option1
+
+#Priorities
+COLLISION_AVOIDANCE_PRIO 1
+AIRBAG_PRIO 1
+ESP_PRIO 2
+
+NOT_USED_FEATURE 10
+
+
+
 class adas_priority(Node):
 
     def __init__(self):
@@ -11,77 +20,93 @@ class adas_priority(Node):
         # Subscribers 
         self.sub_collision = self.create_subscription(MotorsOrderAdas, 'motors_order_collision', self.collision_callback, 10)
         self.sub_esp = self.create_subscription(MotorsOrderAdas,'motors_order_esp', self.esp_callback, 10)
-        self.sub_active_features_hmi = self.create_subscription(HmiFeatures, 'active_features_HMI', self.active_features_HMI_callback, 10)
+        self.sub_active_features_hmi = self.create_subscription(HmiFeatures, 'active_features_hmi', self.active_features_HMI_callback, 10)
+        self.sub_airbag = self.create_subscription(Bool, 'isShockDetected', self.airbag_callback, 10)
 
         # Publishers
         self.pub_final = self.create_publisher(MotorsOrder, 'motors_order', 10)
-        self.pub_features_changes = self.create_publisher(HMIFeatures, 'features_changing_motors_order', 10)
+        self.pub_features_priorities = self.create_publisher(HmiFeatures, 'features_priority_hmi', 10)
 
         # Last msgs of each topic
         self.last_collision = None   
         self.last_esp = None
 
-        # Active features
-        self.collision_avoidance_active_HMI = False
-        self.esp_active_HMI = False
+        # Active features on HMI
+        self.collision_avoidance_active_HMI = 0
+        self.esp_active_HMI = 0
+        self.airbag_active_HMI = 0
 
         #Features changing motors_order
-        self.collision_avoidance_changes = False
-        self.esp_changes = False
+        self.collision_priority = COLLISION_AVOIDANCE_PRIO
+        self.esp_priority = ESP_PRIO
+        self.airbag_priority = AIRBAG_PRIO
 
         self.get_logger().info("adas_priority READY")
 
     # Collision avoidance callback
     def collision_callback(self, msg):
         self.last_collision = msg
-        self.collision_avoidance_changes = self.last_collision.changes # Collision_avoidance did change motors_order or not (to send it to HMI)
-        self.publish_decision()
+        if msg.changes and self.collision_avoidance_active_HMI:
+            self.collision_priority = COLLISION_AVOIDANCE_PRIO
+        else:
+            self.collision_priority = NOT_USED_FEATURE
+
+        self.publish_motors_order_decision()
 
     # ESP callback
     def esp_callback(self, msg):
         self.last_esp = msg
-        self.esp_changes = self.last_esp.changes # ESP did change motors_order or not (to send it to HMI)
-        self.publish_decision()
+        if msg.changes and self.esp_active_HMI:
+            self.esp_priority = ESP_PRIO 
+        else:
+            self.esp_priority = NOT_USED_FEATURE
+        
+        self.publish_motors_order_decision()
 
+    #AIRBAG callback
+    def airbag_callback(self, msg):
+        if msg and self.airbag_active_HMI:
+            self.airbag_priority = AIRBAG_PRIO
+        else:
+            self.airbag_priority = NOT_USED_FEATURE
+        
     # Active features HMI callback
     def active_features_HMI_callback(self, msg):
         self.collision_avoidance_active_HMI = msg.collision_avoidance
         self.esp_active_HMI = msg.esp
+        self.airbag_active_HMI = msg.airbag
 
 
+    def publish_motors_order_decision(self):
+        candidates = []
 
-    def publish_decision(self):
+        if self.last_collision is not None:
+            candidates.append((self.collision_priority, self.last_collision))
 
-        #Priority order to respect: TODO: Add other features in priority list when implemented
-        # 1) Collision Avoidance
-        # 2) ESP
-        # 3) ...
-        # Last ) Any msg can be valid (all contain morors_order_raw)
+        if self.last_esp is not None:
+            candidates.append((self.esp_priority, self.last_esp))
+
+        if not candidates:
+            return
+
+        selected_priority, selected_msg = min(candidates, key=lambda x: x[0])
+
         msg = MotorsOrder()
-        if self.last_collision is not None and self.last_collision.changes and self.collision_avoidance_active_HMI:
-            msg.right_rear_pwm = self.last_collision.right_rear_pwm
-            msg.left_rear_pwm = self.last_collision.left_rear_pwm
-            msg.steering_angle = self.last_collision.steering_angle
-        
-        # TODO: Decomment this part when ESP implemented
-        # elif self.last_esp is not None and self.last_esp.changes and self.esp_active_HMI:
-        #     msg.right_rear_pwm = self.last_esp.right_rear_pwm
-        #     msg.left_rear_pwm = self.last_esp.left_rear_pwm
-        #     msg.steering_angle = self.last_esp.steering_angle
-        
-        else:
-            msg.right_rear_pwm = self.last_collision.right_rear_pwm
-            msg.left_rear_pwm = self.last_collision.left_rear_pwm
-            msg.steering_angle = self.last_collision.steering_angle
-        
-        if msg in not None:
-            self.pub_final.publish(msg)
-            changes_msg = HMI_features()
-            changes_msg.collision_avoidance = self.collision_avoidance_changes
-            changes_msg.airbag = False
-            changes_msg.esp = self.esp_changes
+        msg.right_rear_pwm = selected_msg.right_rear_pwm
+        msg.left_rear_pwm = selected_msg.left_rear_pwm
+        msg.steering_angle = selected_msg.steering_angle
 
-            self.pub_features_changes.publish(changes_msg)
+        self.pub_final.publish(msg)
+
+        # Publishing priorities for HMI use
+        prio_msg = HmiFeatures()
+        prio_msg.collision_avoidance = self.collision_priority
+        prio_msg.airbag = self.airbag_priority
+        prio_msg.esp = self.esp_priority
+
+        self.pub_features_priorities.publish(prio_msg)
+
+            
         
 
 

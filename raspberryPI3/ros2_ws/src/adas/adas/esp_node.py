4c548572936ecf3f88ab3ccf66eb8bed634357e8
Caroline Nguyen <caroline0084@gmail.com>
2025-12-10 21:31:31 +0100

    Double verification logic added without buffer

--- raspberryPI3/ros2_ws/src/adas/adas/esp_node.py
+++ raspberryPI3/ros2_ws/src/adas/adas/esp_node.py
@@ -1,9 +1,13 @@
+# TO DO : Buffer pour le heading
+# TO DO : La commande
+# TO DO : Double detection avec le heading en premier critère puis timer pour voir si angular velocity
+#               Pour distinguer le tournant du choc
+# T_SAMPLE changer avec les vrais trames reçu
+# TO DO : Ajouter un timer pour sortir de l'état intermédiaire si pas de déviation après un certain temps
 import rclpy
 from rclpy.node import Node
-import math
-
 from interfaces.msg import MotorsOrder, JoystickOrder, ECompass 
-from sensor_msgs.msg import Imu          #IMU
+from sensor_msgs.msg import Imu
 
 #Global Variable
 T_SAMPLE = 0.1 #s ou 100ms
@@ -11,6 +15,8 @@ MIN_DETECTION_DEVIATION = 1  #rad/s
 SPEED_PERCENTAGE = 0.5
 HEADING_TOLERANCE = 3.0            # tolerance for the heading
 MAX_STEER = 127                    # steer range
+SIZE_BUFFER_HEADING = 20
+REF_HEADING_DIFF = 5.0                    # heading difference to activate ESP
 class Esp(Node):
     def __init__(self):
         #Initialization of the node
@@ -33,8 +39,15 @@ class Esp(Node):
         #init variable for command
         self.deviation_rate = None
         self.last_heading = None
-        self.esp_active = False
+        self.heading = None
         self.reference_heading = None
+        self.heading_buffer = []
+        
+        # ESP state variables
+        self.esp_intermediate_state = False
+        self.esp_active = False
+        
+
         self.manual_steer = 0.0 # user input
         self.get_logger().info("esp_node READY")
 
@@ -49,9 +62,46 @@ class Esp(Node):
         self.manual_steer = joystick_order.steer
 
     def imu_callback(self, msg):
-        rotation_rate = msg.angular_velocity.z
-        
+
+        if self.esp_intermediate_state :
+            self.deviation_confirmation(msg)
+
+    def ecompass_callback(self, ecompass: ECompass):
+        self.heading = ecompass.heading     
+        # Initialization of last heading
+        if self.last_heading is None:
+            self.last_heading = self.heading
+            return
+        # Compare heading variation
+        self.compare_heading_variation(self.last_heading, self.heading)
+        # Mise à jour pour la prochaine itération
+        self.last_heading = self.heading
+
+
+
+    # ------------------ ESP Implementation ------------------ 
+
+    def sign(self, num):
+        return -1 if num < 0 else 1
+
+    def compare_heading_variation(self, prev, current):
+        if abs(prev - current) > REF_HEADING_DIFF:
+            self.esp_intermediate_state = True
+            self.get_logger().info("ESP IN INTERMEDIATE STATE — heading change detected")
+
+        elif self.esp_active:
+            # ToDO: Compare the headings buffer to see if the heading is stable
+            self.esp_intermediate_state = False
+            self.esp_active = False
+            self.get_logger().info("ESP DEACTIVATED — heading stable")
+            # Deactivation condition
+            # if abs(self.reference_heading - current) < HEADING_TOLERANCE:
+            #     self.esp_active = False
+            #     self.get_logger().info("ESP DEACTIVATED — heading corrected")
+
+    def deviation_confirmation(self, msg):
         # deviation detection
+        rotation_rate = msg.angular_velocity.z
         if abs(rotation_rate) > MIN_DETECTION_DEVIATION:
             if not self.esp_active and self.last_heading is not None:
                 self.esp_active = True
@@ -60,39 +110,8 @@ class Esp(Node):
                     f"ESP ACTIVATED | ref_heading = {self.reference_heading:.2f}°"
                 )
 
-
-    def ecompass_callback(self,ecompass : ECompass):
-        #TO DO: Get the value of desired_cap
-        heading = ecompass.heading
-        if self.last_heading == None :
-            self.last_heading = heading
-        else:
-            self.deviation_rate = (self.last_heading-heading)/T_SAMPLE    #heading rate calculation
-            self.last_heading = heading         #update for next iteration
-            # Run ESP only when active
-            if self.esp_active:
-                self.trajectory_control()
-
-                # Deactivation condition
-                if abs(self.reference_heading - heading) < HEADING_TOLERANCE:
-                    self.esp_active = False
-                    self.get_logger().info("ESP DEACTIVATED — heading corrected")
-
-
-    # ------------------ ESP Implementation ------------------ 
-
-    def detect_deviation(self, msg: Imu): 
-        #utilisez l'angular velocity plus grand que 0.5
-        angular_velocity = msg.angular_velocity.z
-        if abs(angular_velocity) >= MIN_DETECTION_DEVIATION:
-            return True
-        return False
-
-    def sign(self, num):
-        return -1 if num < 0 else 1
-
     def trajectory_control(self):
-        #Complete change in the command
+        #Complete change in the command with pallier
 
         # Update motors order
         # Value for Motors Control

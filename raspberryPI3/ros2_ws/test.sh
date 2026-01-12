f01965d11a4995acfc3d4cc5a5390aaa7124f7e6
pofpifpaf <adam.gironcel@gmail.com>
2026-01-12 17:23:13 +0100

    test script now executable

--- raspberryPI3/ros2_ws/test.sh
+++ raspberryPI3/ros2_ws/test.sh
@@ -9,4 +9,4 @@ streamlit run hmi.py &
 cd ../../../../
 
 ros2 launch geicar_start geicar.launch.py &
-ros2 run imu_rx ecompass_node.py
+ros2 run imu_rx ecompass_node --ros-args -p mag_offset_x:=-931.5 -p mag_offset_y:=87.0 -p mag_offset_z:=123.0 

130b87e9ca3d82198f1bbea08695b4c17a447e55
pofpifpaf <adam.gironcel@gmail.com>
2026-01-12 16:49:00 +0100

    script test adjustment

--- raspberryPI3/ros2_ws/test.sh
+++ raspberryPI3/ros2_ws/test.sh
@@ -2,7 +2,11 @@
 
 source install/setup.bash
 cd src/hmi/hmi/python_files/
+
 uvicorn backend_api:app --host 0.0.0.0 --port 8000 &
-ros2 launch geicar_start geicar.launch.py &
 streamlit run hmi.py &
+
+cd ../../../../
+
+ros2 launch geicar_start geicar.launch.py &
 ros2 run imu_rx ecompass_node.py

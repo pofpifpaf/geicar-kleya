13ffb233a62fa2586c0345e95df605913ede3cce
pofpifpaf <adam.gironcel@gmail.com>
2026-01-12 17:48:03 +0100

    Hotfix sp4 demo version (#22)
    * fixed hmi node and added scripts
    
    * fixed scripts
    
    * script test adjustment
    
    * test script now executable
    
    * now with a verify script

--- raspberryPI3/ros2_ws/test.sh
+++ raspberryPI3/ros2_ws/test.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
+source install/setup.bash
 cd src/hmi/hmi/python_files/
+
 uvicorn backend_api:app --host 0.0.0.0 --port 8000 &
+streamlit run hmi.py &
+
+cd ../../../../
+
 ros2 launch geicar_start geicar.launch.py &
-streamlit run hmi.py
+ros2 run imu_rx ecompass_node --ros-args -p mag_offset_x:=-931.5 -p mag_offset_y:=87.0 -p mag_offset_z:=123.0 
